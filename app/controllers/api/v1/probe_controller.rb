3b683c5aa2613907cae6a1138cc46c699208d452
André Oliveira <andreoliveirar2d2@gmail.com>
2018-12-14 18:08:38 -0400

    validations if params is null

--- app/controllers/api/v1/probe_controller.rb
+++ app/controllers/api/v1/probe_controller.rb
@@ -2,13 +2,16 @@ module Api
   module V1
     class ProbeController < ApplicationController
       before_action :set_probe
+      before_action :valid_params, only: :moviments
 
       def reset
         render 200, json: {cod: 200, status: "OK", message: "A sonda foi resetada com sucesso"} if @probe.destroy
       end
 
       def moviments
-
+        @moviments.each do |moviment|
+          puts moviment.upcase
+        end
       end
 
       def position
@@ -24,6 +27,11 @@ module Api
 
       def probe_params
         params.permit(:moviments)
+      end
+
+      def valid_params
+        @moviments = [params[:moviments]] unless params[:moviments].nil?
+        render 400, json: {cod: 400, status: "Bad Request", message: "Insira os parametros para mover a sonda"} if @moviments == [""] || params[:moviments].nil?
       end
     end
   end
