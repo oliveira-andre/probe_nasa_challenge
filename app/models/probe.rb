2642c6e212724a3bf6208f7cde08ec64ff573249
André Oliveira <andreoliveirar2d2@gmail.com>
2018-12-15 16:16:22 -0400

    vaidating position in model

--- app/models/probe.rb
+++ app/models/probe.rb
@@ -2,4 +2,12 @@ class Probe < ApplicationRecord
   validates :x_axis, presence: true
   validates :y_axis, presence: true
   validates :direction, presence: true
+  validate  :invalid_position
+
+  def invalid_position
+    errors.add(:x_axis) if x_axis < 0
+    errors.add(:y_axis) if y_axis < 0
+    errors.add(:x_axis) if x_axis > 4
+    errors.add(:y_axis) if y_axis > 4
+  end
 end
