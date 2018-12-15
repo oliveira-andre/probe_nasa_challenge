7cc104f81989eec38a9044ed42209254fc99b4f9
André Oliveira <andreoliveirar2d2@gmail.com>
2018-12-15 16:53:01 -0400

    adding routes to message

--- app/controllers/concerns/auxiliar_methods.rb
+++ app/controllers/concerns/auxiliar_methods.rb
@@ -3,7 +3,11 @@ module AuxiliarMethods
 
   def routes
     routes = {
-        "/api/v1/": "Página principal, que exibe as boas vindas ao jogo"
+        "/api/v1/": "Página principal, que exibe as boas vindas ao jogo",
+        "/api/v1/instructions": "Exibe as instruções do jogo",
+        "/api/v1/probe/moviments": "Aqui o jogo começa, e você pode movimentar sua sonda",
+        "/api/v1/probe/reset": "Reseta a posição da sua sonda",
+        "/api/v1/probe/position": "Exibe em qual posição sua sonda está"
     }
     routes
   end
