782beccd270f8356eb9c7adad2ef70ffee5a5622
André Oliveira <andreoliveirar2d2@gmail.com>
2018-12-15 16:35:47 -0400

    putting the condition into pattern rubocop

--- app/controllers/concerns/probe_moviments.rb
+++ app/controllers/concerns/probe_moviments.rb
@@ -3,14 +3,23 @@ module ProbeMoviments
 
   def turn_around(spin)
     probe = Probe.last
-    probe.update(direction: "C") if probe.direction == "D" && spin == "GE"
-    probe.update(direction: "B") if probe.direction == "D" && spin == "GD"
-    probe.update(direction: "E") if probe.direction == "C" && spin == "GE"
-    probe.update(direction: "D") if probe.direction == "C" && spin == "GD"
-    probe.update(direction: "B") if probe.direction == "E" && spin == "GE"
-    probe.update(direction: "C") if probe.direction == "E" && spin == "GD"
-    probe.update(direction: "D") if probe.direction == "B" && spin == "GE"
-    probe.update(direction: "E") if probe.direction == "B" && spin == "GD"
+    times = 0
+    probe.update(direction: "C") if probe.direction == "D" && spin == "GE" && times.zero?
+    times += 1
+    probe.update(direction: "B") if probe.direction == "D" && spin == "GD" && times.zero?
+    times += 1
+    probe.update(direction: "E") if probe.direction == "C" && spin == "GE" && times.zero?
+    times += 1
+    probe.update(direction: "D") if probe.direction == "C" && spin == "GD" && times.zero?
+    times += 1
+    probe.update(direction: "B") if probe.direction == "E" && spin == "GE" && times.zero?
+    times += 1
+    probe.update(direction: "C") if probe.direction == "E" && spin == "GD" && times.zero?
+    times += 1
+    probe.update(direction: "D") if probe.direction == "B" && spin == "GE" && times.zero?
+    times += 1
+    probe.update(direction: "E") if probe.direction == "B" && spin == "GD" && times.zero
+    times += 1
   end
 
   def move
@@ -22,7 +31,7 @@ module ProbeMoviments
   end
 
   def valid_moviments(moviment)
-    error = true unless moviment == "GE" || moviment== "GD" || moviment == "M"
+    error = true unless moviment == "GE" || moviment == "GD" || moviment == "M"
     error
   end
 
